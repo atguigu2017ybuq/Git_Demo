fc0bba7a2594ba3492d0561a1d98320e4d74513a
atguigu2017ybuq <atguigu_2017_ybuq@126.com>
2017-11-22 22:21:00 +0800

    我为彦祖代言！

--- src/main/java/com/atguigu/git/GitDemoTest.java
+++ src/main/java/com/atguigu/git/GitDemoTest.java
@@ -1,7 +1,12 @@
 package com.atguigu.git;
 
 public class GitDemoTest {
+	
 	//这个是准备提交的注释
-  //这个注释是用于比较文件
-  private String name;
+    //这个注释是用于比较文件
+    private String name;
+    
+    public static void main(String[] args) {
+		System.out.println("我是彦祖，我为自己代言！");
+	}
 }
